bb3ef9546aad9ad6211b9c8055335d069e353c62
neethupath <pathnee222gmail.com>
2023-03-31 12:25:09 -0400

    function

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// code your solution here
+
+
+function saturdayFun(activity)
+   {
+   if(activity === 'bathe my dog')
+   return ('This Saturday, I want to bathe my dog!');
+   else
+   return ('This Saturday, I want to roller-skate!');
+}
+function mondayWork(activity)
+{
+    if(activity === 'work from home')
+    return ('This Monday, I will work from home.');
+    else
+    return('This Monday, I will go to the office.');
+}
+const st=wrapAdjective('*')();
+console.log(st);
+function wrapAdjective(activity)
+{
+let special;
+
+    if(activity==='*')
+    {
+        special='a hard worker';
+    return function(special){
+        return (`You are *${special}*!`);
+    };
+
+
+    }
+    else if(activity==='||')
+    {
+        special='a dedicated programmer';
+     return function(special)
+     {
+        return (`You are ||${special}||!`);
+     };
+    }
+
+}
